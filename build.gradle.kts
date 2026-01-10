64badd20a1b7206efef20576344a062971c101fc
agilis allievo <aegliv@gmail.com>
2026-01-10 15:15:16 +0100

    Updates project to support JDK 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.6.0"
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
 }
 
 group = "com.gha-workflow-plugins.run-syntax-support"
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        create("IU", "2024.3")
+        create("IU", "2025.1.4.1")
         bundledPlugin("org.jetbrains.plugins.yaml")
 
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
